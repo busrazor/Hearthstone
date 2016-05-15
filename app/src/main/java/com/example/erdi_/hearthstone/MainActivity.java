a7fd998c8c8044ae56d4ff08d889ddb183b323ab
kato6661 <busrazor@gmail.com>
2016-05-15 14:52:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/erdi_/hearthstone/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.erdi_.hearthstone;
+
+import android.content.Intent;
+import android.media.SoundPool;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    SoundPool sp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
